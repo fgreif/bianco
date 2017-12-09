ad82e8926e1de1afae7a9cc325ca7e705e7df571
Florian <flo.g95@gmail.com>
2017-12-09 21:41:49 +0100

    file input/output done (?), markov started

---
+++ MarkovTextGen/src/markov/MarkovChain.java
@@ -0,0 +1,25 @@
+package markov;
+import files.*;
+
+import java.util.Map;
+
+/**
+ * Created by Florian on 09.12.2017.
+ */
+public class MarkovChain {
+    private Map<String, Map<String, Integer>> frequencyTable;
+
+    public MarkovChain() {
+
+    }
+
+    private void fillFrequencyTable(String inputFileName, String outputFileName) { {
+
+    }
+
+    }
+
+    public Map<String, Map<String, Integer>> getFrequencyTable() {
+        return frequencyTable;
+    }
+}
